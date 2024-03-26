902a2b1606755179a83e266c137f013904faf477
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-03-27 00:39:58 +0700

    add dataset

--- app/page.tsx
+++ app/page.tsx
@@ -33,9 +33,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-12">
       <h1 className="text-5xl font-bold mb-2 text-center">Transformers.js</h1>
-      <h2 className="text-2xl mb-4 text-center">
-        Next.js template (server-side)
-      </h2>
+      <h2 className="text-2xl mb-4 text-center">Next.js (server-side)</h2>
+      <h2 className="text-xl mb-4 text-center">BERT Fine Tuning</h2>
       <input
         type="text"
         className="w-full max-w-xs p-2 border border-gray-300 rounded mb-4"
